9dbebf1c25d16a77379e7306038272a5a51d0ddd
Kirupashree V <vkirupa1802@gmail.com>
2025-09-28 13:05:12 +0530

    Create register-adopter.component.ts

---
+++ src/app/register-adopter/register-adopter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-adopter',
+  imports: [],
+  templateUrl: './register-adopter.component.html',
+  styleUrl: './register-adopter.component.css'
+})
+export class RegisterAdopterComponent {
+
+}
